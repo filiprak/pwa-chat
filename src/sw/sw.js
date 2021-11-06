65e6e37a7fdccddfddb63d769ad97122b538880e
filiprak <filiprak@wp.pl>
2021-11-07 00:07:46 +0100

    Cleanup

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -15,10 +15,10 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
-onBackgroundMessage(messaging, (payload) => {
+onBackgroundMessage(messaging, async (payload) => {
     console.log('[sw.js] Received background message ', payload);
 
-    self.registration.showNotification(payload.data?.title || '', {
+    await self.registration.showNotification(payload.data?.title || '', {
         body: payload.data?.body,
         icon: payload.data?.icon,
         vibrate: payload.data && payload.data.vibrate ? JSON.parse(payload.data.vibrate) : undefined,
