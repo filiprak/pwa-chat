06004bcbfd96e8dbe9ba351991fb29cc2e194391
filiprak <filiprak@wp.pl>
2021-11-07 01:12:27 +0100

    Fix double messages

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -15,13 +15,40 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
-onBackgroundMessage(messaging, async (payload) => {
-    console.log('[sw.js] Received background message ', payload);
-
-    await self.registration.showNotification(payload.data?.title || '', {
-        body: payload.data?.body,
-        icon: payload.data?.icon,
-        vibrate: payload.data && payload.data.vibrate ? JSON.parse(payload.data.vibrate) : undefined,
-        priority: payload.data.priority,
+const hasVisibleClients = (clientList) => {
+    return clientList.some(client => client.visibilityState === 'visible' &&
+        !client.url.startsWith('chrome-extension://'));
+};
+const getClientList = () => {
+    return self.clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true,
     });
+};
+
+const onPush = async (event) => {
+    console.log('[sw.js] Received push message ', event.data);
+
+    let payload;
+
+    try {
+        payload = event.data ? event.data.json() : null;
+    } catch (err) {
+        payload = null;
+    }
+
+    const clients = await getClientList();
+
+    if (payload && !hasVisibleClients(clients)) {
+        await self.registration.showNotification(payload.data?.title || '', {
+            body: payload.data?.body,
+            icon: payload.data?.icon,
+            vibrate: payload.data && payload.data.vibrate ? JSON.parse(payload.data.vibrate) : undefined,
+            priority: payload.data.priority,
+        });
+    }
+};
+
+self.addEventListener('push', (event) => {
+    event.waitUntil(onPush(event));
 });
--- tests/manual/fcm.js
+++ tests/manual/fcm.js
@@ -25,6 +25,7 @@ admin
         tokens: [
             'cwdwI05hwLeuG6S3rM26gd:APA91bEugI-B7SIuAhlIhhjevTicRma3Nrg6RdMmRv6CIgMH5UwJ0sPrf3EBdxrEi4F_0uNa_ZyWkpYmukuhU4lGaHZoEl-CC280aPCiyQrGR9K50ahvP1U7wsUEfoMzNFaH8rgqFlJX',
             'fOvqhj9N3RhEUfYqfVZiF7:APA91bFjYuq36FGAoJTlqqnpaU2v5Ktw0zywf6szufuiLgVvLuEHYXegPCvIkKntW7rZxp6hNcrmqjQU1JCzeBf_OEmvnJDLbMPBB5sF4PJm-2WrRpvP6vYog8crhdHbWob0CeA3A9tM',
+            'cfH6JaqoVuxqHPfSXWEeeL:APA91bGVTCsWzwfMIKXLQAjvA34OzAznJP17ouxPc0Z8ghPPVTb8RAALc2DwlxB4fmQc5sdVOPY7gLfeptMqEF4uzXgxPPkvn4UnFNQH4KRjcV5kWxDFOxrSn4eJS4i_5acYgoH-NFPy',
         ],
     })
     .then((response) => {
